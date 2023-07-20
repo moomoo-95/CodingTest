82354764bce4f4e7e47d4ca0bd8e4b5eb940110b
moomoo-95 <gtws2081@daum.net>
2023-07-21 00:06:48 +0900

    init commit

---
+++ coding_test/src/main/java/baekjoon/Main.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) {
+        // 제출할 메서드 추가
+    }
+
+    private static String readInput() {
+        try { return reader.readLine(); } catch (Exception e){ return ""; }
+    }
+
+    private static void writeOutput(String output) {
+        try {
+            writer.write(output);
+            writer.flush();
+        } catch (IOException ignored) {/* ignored */}
+    }
+}
