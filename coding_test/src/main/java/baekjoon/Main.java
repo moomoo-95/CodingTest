fb4067dd45716a8fa7f9a6a4899833aeec127632
moomoo-95 <gtws2081@daum.net>
2023-08-01 22:28:28 +0900

    10101 삼각형 외우기

--- coding_test/src/main/java/baekjoon/Main.java
+++ coding_test/src/main/java/baekjoon/Main.java
@@ -1,31 +1,13 @@
 package baekjoon;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) {
-        // 제출할 메서드 추가
-        char[][] stars = new char[][] {
-                {'*', '*', '*'},
-                {'*', ' ', '*'},
-                {'*', '*', '*'}
-        };
-
-        char[][] result = new char[9][9];
-
-        char[][] lineA = new char[3][9];
-        for (int idx =0; idx<stars.length; idx++){
-            System.arraycopy(stars[idx], 0, lineA[idx], 0, stars[idx].length);
-            System.arraycopy(stars[idx], 0, lineA[idx], stars[idx].length, stars[idx].length);
-            System.arraycopy(stars[idx], 0, lineA[idx], stars[idx].length*2, stars[idx].length);
-        }
-
-        System.arraycopy(stars, 0, result, 0, stars.length);
-        System.arraycopy(stars, 0, result, stars.length, stars.length);
+        // test main
         stop();
     }
 
