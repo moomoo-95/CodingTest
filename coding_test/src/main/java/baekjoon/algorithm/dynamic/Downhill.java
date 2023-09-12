04a55d5affe31cd0fccad36ce9657eb0168995bd
HyeonSeong Lim <gtws2081@daum.net>
2023-09-12 10:13:54 +0900

    1520 내리막길

--- coding_test/src/main/java/baekjoon/algorithm/dynamic/Downhill.java
+++ coding_test/src/main/java/baekjoon/algorithm/dynamic/Downhill.java
@@ -1,4 +1,4 @@
-package baekjoon.temp;
+package baekjoon.algorithm.dynamic;
 
 import java.io.*;
 import java.util.StringTokenizer;
@@ -10,16 +10,16 @@ import java.util.StringTokenizer;
  * 그래프 탐색
  * 깊이 우선 탐색
  * [Result]
- * 메모리 : 0 kb
- * 수행시간 : 0 ms
+ * 메모리 : 34984 kb
+ * 수행시간 : 388 ms
  */
 public class Downhill {
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
     private static int[][] hill;
+    private static int[][] ways;
     private static int m;
     private static int n;
-    private static int result = 0;
 
     public static void main(String[] args) {
         downhill();
@@ -49,24 +49,25 @@ public class Downhill {
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
         hill = new int[m][n];
+        ways = new int[m][n];
         for (int i=0;i<m;i++){
             st = new StringTokenizer(readInput(), " ");
             for (int j=0;j<n;j++){
                 hill[i][j] = Integer.parseInt(st.nextToken());
+                ways[i][j]--;
             }
         }
-        findWay(0, 0);
-        writeOutput(String.valueOf(result));
+        writeOutput(String.valueOf(findWay(0, 0)));
     }
 
-    private static void findWay(int x, int y){
-        if(x == m-1 && y == n-1) {
-            result++;
-            return;
-        }
-        if(x > 0 && hill[x][y] > hill[x-1][y]) findWay(x-1, y);
-        if(y > 0 && hill[x][y] > hill[x][y-1]) findWay(x, y-1);
-        if(x < m-1 && hill[x][y] > hill[x+1][y]) findWay(x+1, y);
-        if(y < n-1 && hill[x][y] > hill[x][y+1]) findWay(x, y+1);
+    private static int findWay(int x, int y){
+        if(x == m-1 && y == n-1) return 1;
+        if(ways[x][y] != -1) return ways[x][y];
+        ways[x][y] = 0;
+        if(x > 0 && hill[x][y] > hill[x-1][y]) ways[x][y] += findWay(x-1, y);
+        if(y > 0 && hill[x][y] > hill[x][y-1]) ways[x][y] += findWay(x, y-1);
+        if(x < m-1 && hill[x][y] > hill[x+1][y]) ways[x][y] += findWay(x+1, y);
+        if(y < n-1 && hill[x][y] > hill[x][y+1]) ways[x][y] += findWay(x, y+1);
+        return ways[x][y];
     }
 }
