b5aa763cb2509d16cae0b2da46d8861325b7d8d4
moomoo-95 <gtws2081@daum.net>
2023-09-28 20:43:01 +0900

    1766_문제집

--- coding_test/src/main/java/baekjoon/algorithm/graphsearch/Workbook.java
+++ coding_test/src/main/java/baekjoon/algorithm/graphsearch/Workbook.java
@@ -1,19 +1,23 @@
-package baekjoon.temp;
+package baekjoon.algorithm.graphsearch;
 
 import java.io.*;
 import java.util.*;
 
 /**
  * [Algorithm]
- *
+ * 자료 구조
+ * 그래프 이론
+ * 우선순위 큐
+ * 위상 정렬
  * [Result]
- * 메모리 : 0 kb
- * 수행시간 : 0 ms
+ * 메모리 : 45612 kb
+ * 수행시간 : 508 ms
  */
 public class Workbook {
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
     private static final StringBuilder SB = new StringBuilder();
+    private static int n;
 
     public static void main(String[] args) {
         workbook();
@@ -40,16 +44,41 @@ public class Workbook {
 
     private static void workbook() {
         StringTokenizer st = new StringTokenizer(readInput(), " ");
-        int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        Map<Integer, Integer> orderWorks = new HashMap<>();
-        for (int i=1;i<=n;i++){
-            orderWorks.put(i, 0);
+        List<List<Integer>> edgeList = new ArrayList<>();
+        int[] inEdgeCount = new int[n];
+        for(int i=0;i<n;i++){
+            edgeList.add(new ArrayList<>());
         }
+
         for (int i=0;i<m;i++){
             st = new StringTokenizer(readInput(), " ");
-            orderWorks.replace(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            edgeList.get(a-1).add(b-1);
+            inEdgeCount[b-1]++;
         }
+        topologicalSort(inEdgeCount, edgeList);
         writeOutput(SB.toString());
+    }
+
+    private static void topologicalSort(int[] inEdgeCount, List<List<Integer>> edgeList){
+        PriorityQueue<Integer> zeroQ = new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            if(inEdgeCount[i] == 0) zeroQ.offer(i);
+        }
+
+        while (!zeroQ.isEmpty()){
+            int node = zeroQ.poll();
+            SB.append(node+1).append(" ");
+
+            for(Integer i:edgeList.get(node)){
+                inEdgeCount[i]--;
+                if(inEdgeCount[i] == 0){
+                    zeroQ.offer(i);
+                }
+            }
+        }
     }
 }
