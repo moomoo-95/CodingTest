f9625cfd461b68de3fc4d0b7f36fb588cf23ed4b
moomoo-95 <gtws2081@daum.net>
2023-10-21 13:34:35 +0900

    1340_연도 진행바

--- coding_test/src/main/java/baekjoon/temp/TestFormat.java
+++ coding_test/src/main/java/baekjoon/temp/TestFormat.java
@@ -20,23 +20,9 @@ public class TestFormat {
         stop();
     }
 
-    private static String readInput() {
-        try { return READER.readLine(); } catch (Exception e){ return ""; }
-    }
-
-    private static void writeOutput(String output) {
-        try {
-            WRITER.write(output);
-            WRITER.flush();
-        } catch (IOException ignored) {/* ignored */}
-    }
-
-    private static void stop(){
-        try {
-            READER.close();
-            WRITER.close();
-        } catch (IOException ignored) {/* ignored */}
-    }
+    private static String readInput() { try { return READER.readLine(); } catch (Exception e){ return ""; } }
+    private static void writeOutput(String output) { try { WRITER.write(output); WRITER.flush(); } catch (IOException ignored) {/* ignored */} }
+    private static void stop(){ try { READER.close(); WRITER.close(); } catch (IOException ignored) {/* ignored */} }
 
     private static void testFormat() {
         StringTokenizer st = new StringTokenizer(readInput(), " ");
