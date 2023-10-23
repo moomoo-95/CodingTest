0d74c935f24dd4527ffb483b50754bc3b75bdf4e
HyeonSeong Lim <gtws2081@daum.net>
2023-10-23 16:23:53 +0900

    이차원 행렬 연산 틀

--- coding_test/src/main/java/baekjoon/temp/TestFormat.java
+++ coding_test/src/main/java/baekjoon/temp/TestFormat.java
@@ -15,10 +15,7 @@ public class TestFormat {
     private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
     private static final StringBuilder SB = new StringBuilder();
 
-    public static void main(String[] args) {
-        testFormat();
-        stop();
-    }
+    public static void main(String[] args) { testFormat(); stop(); }
 
     private static String readInput() { try { return READER.readLine(); } catch (Exception e){ return ""; } }
     private static void writeOutput(String output) { try { WRITER.write(output); WRITER.flush(); } catch (IOException ignored) {/* ignored */} }
