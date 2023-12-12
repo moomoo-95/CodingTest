d9e6d37e6e4ee82ef39b2ace24abcc1761db91d8
moomoo-95 <gtws2081@daum.net>
2023-12-13 00:17:39 +0900

    퍼즐 틀

---
+++ coding_test/src/main/java/baekjoon/temp/Puzzle.java
@@ -0,0 +1,28 @@
+package baekjoon.temp;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * [Algorithm]
+ *
+ * [Result]
+ * 메모리 : 0 kb
+ * 수행시간 : 0 ms
+ */
+public class Puzzle {
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder SB = new StringBuilder();
+
+    public static void main(String[] args) { puzzle(); stop(); }
+
+    private static String readInput() { try { return READER.readLine(); } catch (Exception e){ return ""; } }
+    private static void writeOutput(String output) { try { WRITER.write(output); WRITER.flush(); } catch (IOException ignored) {/* ignored */} }
+    private static void stop(){ try { READER.close(); WRITER.close(); } catch (IOException ignored) {/* ignored */} }
+
+    private static void puzzle() {
+        StringTokenizer st = new StringTokenizer(readInput(), " ");
+        writeOutput(SB.toString());
+    }
+}
