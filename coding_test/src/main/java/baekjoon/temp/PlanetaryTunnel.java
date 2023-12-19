f30af3b3b9cbd6a6d2e766f24d76f20a5ee4d90b
moomoo-95 <gtws2081@daum.net>
2023-12-19 22:53:57 +0900

    행성터널 메모리초과

--- coding_test/src/main/java/baekjoon/temp/PlanetaryTunnel.java
+++ coding_test/src/main/java/baekjoon/temp/PlanetaryTunnel.java
@@ -13,10 +13,9 @@ import java.util.*;
 public class PlanetaryTunnel {
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static Integer[][] pArray;
-    private static Map<Set<Integer>, Integer> tMap = new HashMap();
+    private static Map<Tunnel, Integer> tMap = new HashMap();
     private static Map<Integer, Set<Integer>> pSets = new HashMap();
-    private static final int cost = 0;
+    private static int cost = 0;
 
 
     public static void main(String[] args) { planetaryTunnel(); stop(); }
@@ -26,8 +25,49 @@ public class PlanetaryTunnel {
     private static void stop(){ try { READER.close(); WRITER.close(); } catch (IOException ignored) {/* ignored */} }
 
     private static void planetaryTunnel() {
+        parsingInput();
+
+        // 제일 싼 터널 추출 및 비용 추가
+        while (pSets.size()!=1){
+            Tunnel minT = tMap.keySet().stream().min((Comparator.comparingInt(o -> tMap.get(o)))).get();
+            Integer dist = tMap.remove(minT);
+            cost+=dist;
+            // 두 행성 Set K,V 추출 및 두 행성 Set 합침
+            Integer aK = null, bK = null;
+            Set<Integer> aS = null, bS = null;
+            for(Map.Entry<Integer, Set<Integer>> e:pSets.entrySet()){
+                if(e.getValue().contains(minT.x)) { aK = e.getKey();aS = e.getValue(); }
+                if(e.getValue().contains(minT.y)) { bK = e.getKey();bS = e.getValue(); }
+                if(aK!=null&&bK!=null) {
+                    pSets.remove(bK);
+                    aS.addAll(bS);
+                    break;
+                }
+            }
+            // 행성 Set 과 또다른 행성 Set 간 중복된 간선 존재 시 싼 간선 이외 제거
+            Set<Tunnel> keys = new HashSet<>(tMap.keySet());
+            for (Tunnel k:keys){
+                if(k.x==bK) {
+                    Integer nCost = tMap.remove(k);
+                    k.x = aK;
+                    if(k.x>k.y){int tmp = k.x;k.x=k.y;k.y=tmp;}
+                    if(tMap.containsKey(k)&& tMap.get(k)>nCost) tMap.replace(k, nCost);
+                } else if(k.y==bK) {
+                    Integer nCost = tMap.remove(k);
+                    k.y = aK;
+                    if(k.x>k.y){int tmp = k.x;k.x=k.y;k.y=tmp;}
+                    if(tMap.containsKey(k)&& tMap.get(k)>nCost) {
+                        tMap.replace(k, nCost);
+                    }
+                }
+            }
+        }
+        writeOutput(String.valueOf(cost));
+    }
+
+    private static void parsingInput(){
         int n = Integer.parseInt(readInput());
-        pArray = new Integer[n][3];
+        Integer[][] pArray = new Integer[n][3];
 
         StringTokenizer st;
         for(int i=0;i<n;i++){
@@ -39,29 +79,23 @@ public class PlanetaryTunnel {
         }
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                Set<Integer> t = new HashSet<>();
-                t.add(i);t.add(j);
+                Tunnel t = new Tunnel(i,j);
                 tMap.put(t, Math.min(Math.min(Math.abs(pArray[i][0]-pArray[j][0]), Math.abs(pArray[i][1]-pArray[j][1])), Math.abs(pArray[i][2]-pArray[j][2])));
             }
         }
-        // 메서드화 할 예정(반복)
-        Set<Integer> minK = tMap.keySet().stream().min((Comparator.comparingInt(o -> tMap.get(o)))).get();
-        Integer[] minT = minK.toArray(new Integer[2]);
-        Integer dist = tMap.remove(minK);
-        Integer aK = null, bK = null;
-        Set<Integer> aS = null, bS = null;
-        for(Integer k:pSets.keySet()){
-            Set<Integer> s = pSets.get(k);
-            if(s.contains(minT[0])) { aK = k;aS = s; }
-            if(s.contains(minT[1])) { bK = k;bS = s; }
-            if(aK!=null&&bK!=null) {
-                aS.addAll(bS);
-                pSets.remove(bK);
-                break;
-            }
+    }
+
+    static class Tunnel{
+        int x, y;
+        Tunnel(int x, int y){this.x=x;this.y=y;}
+        @Override
+        public boolean equals(Object o) {
+            Tunnel po = (Tunnel) o;
+            return this.x == po.x && this.y == po.y;
         }
-
-
-        writeOutput(String.valueOf(cost));
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(x*100000+y);
+        }
     }
 }
