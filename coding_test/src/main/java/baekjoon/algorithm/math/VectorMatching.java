677ffdbcd1c5634b0c6632a4cab5e0e538c961dd
HyeonSeong Lim <gtws2081@daum.net>
2023-12-27 09:56:25 +0900

    1007_벡터 매칭

--- coding_test/src/main/java/baekjoon/algorithm/math/VectorMatching.java
+++ coding_test/src/main/java/baekjoon/algorithm/math/VectorMatching.java
@@ -1,19 +1,22 @@
-package baekjoon.temp;
+package baekjoon.algorithm.math;
 
 import java.io.*;
 import java.util.StringTokenizer;
 
 /**
  * [Algorithm]
- *
+ * 수학
+ * 브루트포스 알고리즘
  * [Result]
- * 메모리 : 0 kb
- * 수행시간 : 0 ms
+ * 메모리 : 14900 kb
+ * 수행시간 : 224 ms
  */
 public class VectorMatching {
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
     private static final StringBuilder SB = new StringBuilder();
+    private static double result = Double.MAX_VALUE;
+    private static int[][] points;
 
     public static void main(String[] args) { vectorMatching(); stop(); }
 
@@ -26,12 +29,28 @@ public class VectorMatching {
         StringTokenizer st;
         for(int i=0;i<t;i++){
             int n = Integer.parseInt(readInput());
-            int[][] points = new int[n][2];
+            result = Double.MAX_VALUE;
+            points = new int[n][2];
+            int x = 0;
+            int y = 0;
             for (int j=0;j<n;j++){
                 st = new StringTokenizer(readInput(), " ");
                 points[j] = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+                x+=points[j][0];
+                y+=points[j][1];
             }
+            combination(0, 0, x, y);
+            SB.append(String.format("%.12f", result)).append("\n");
         }
         writeOutput(SB.toString());
+    }
+
+    private static void combination(int cnt, int idx, int x, int y) {
+        if(cnt==points.length/2) {
+            result = Math.min(result, Math.sqrt((double)x*x + (double)y*y) );
+            return;
+        }
+        for (int i = idx; i < points.length; i++)
+            combination(cnt+1, i+1, x-2*points[i][0], y-2*points[i][1]);
     }
 }
